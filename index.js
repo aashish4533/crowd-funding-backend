a17d8bce7a919c0a79975cd4ba317c31cb70641a
Aashish <k224533@nu.edu.pk>
2024-12-03 17:43:39 +0500

    check1

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-const express = require('express');
+// const express = require('express');
 // const bodyParser = require('body-parser');
-const pool = require('./db.js');  
-const path = require('path');
-const routes = require('./routes/index.js'); 
+// const pool = require('./db.js');  
+// const path = require('path');
+// const routes = require('./routes/index.js'); 
 
 
-const app = express();
-const PORT = process.env.PORT || 5000;  
+// const app = express();
+// const PORT = process.env.PORT || 5000;  
 
-// Middleware
+// // Middleware
 // app.use(bodyParser.json());
 
 
-// Use the routes from routes/index.js
-app.use('/api', routes);  // All routes prefixed with '/api'
+// // Use the routes from routes/index.js
+// app.use('/api', routes);  // All routes prefixed with '/api'
 
-// Route to test the server
+// // Route to test the server
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the Crowdfunding API!');
+// });
+
+// // Route to test database connection
+// app.get('/test-db', async (req, res) => {
+//     try {
+//         const result = await pool.query('SELECT NOW()');  // Query to check the current time in the database
+//         res.json({ message: 'Database connected successfully!', serverTime: result.rows[0] });
+//     } catch (err) {
+//         console.error('Database connection error:', err);
+//         res.status(500).json({ error: 'Database connection failed' });
+//     }
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+const express = require('express')
+const app = express()
+const port = 3000
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the Crowdfunding API!');
-});
+  res.send('Hello World!')
+})
 
-// Route to test database connection
-app.get('/test-db', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT NOW()');  // Query to check the current time in the database
-        res.json({ message: 'Database connected successfully!', serverTime: result.rows[0] });
-    } catch (err) {
-        console.error('Database connection error:', err);
-        res.status(500).json({ error: 'Database connection failed' });
-    }
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
